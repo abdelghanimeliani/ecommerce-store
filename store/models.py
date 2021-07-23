a3c4726a78a1ab656feb0d7f03fdd34a1f987b85
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-07-23 03:36:08 +0200

    add models and migrite the database

--- store/models.py
+++ store/models.py
@@ -1,3 +1,46 @@
+from enum import auto
+from django import db
 from django.db import models
-
+from django.db.models.fields import DateField
+from django.contrib.auth.models import User 
 # Create your models here.
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255 , db_index=True)
+    slug = models.SlugField(max_length=255 , unique=True)  
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reserve('store:category_list' ,args[self.slug])
+
+    def __str__(self) :
+        return self.name
+        
+
+class Product(models.Model): 
+    category = models.ForeignKey(Category,related_name='product',on_delete=models.CASCADE) 
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE , related_name="product_creator")
+    title = models.CharField(max_length=255 )
+    models.CharField(max_length=255 , default="admin")
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255 )
+    price= models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        ordering = ['-created']
+
+    def __str__(self) :
+        return self.title 
+    
+
