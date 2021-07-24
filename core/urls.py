42271cede1b2319b680143bfe4268ade8e27e514
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-07-24 04:05:00 +0200

    add the views and the urls

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from django.conf import settings 
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('store.urls'), name='store'), 
 ]
 
 if settings.DEBUG : 
--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Category ,Product 
 # Create your views here.
+
+def all_products(request):
+    product = Product.objects.all()
+    return render(request  , 'store/home.html' , {'products' : product})
+    
