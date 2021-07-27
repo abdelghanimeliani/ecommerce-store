b3416b2d425db99236063cebd117d2efe5c15ec0
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-07-27 18:17:47 +0200

    add a detail page

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='store'
 urlpatterns = [
     path('', views.all_products , name='all_products'),
-
+      path('item/<slug:slug>/', views.product_detail, name='product_detail'),
+    
 ]
--- store/views.py
+++ store/views.py
@@ -13,14 +13,10 @@ def categories(request):
         "categories": Category.objects.all(),
     }
 
-"""
-def category_list(request, category_slug=None):
-    category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', {'category': category, 'products': products})
-
 
 def product_detail(request, slug):
+    '''
+    a revoir
+    '''
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/single.html', {'product': product})
-"""
+    return render(request, 'store/products/detail.html', {'product': product})
