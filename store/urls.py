50ff4c72e92c195c0712a23c8c7da556edf5a15b
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-07-28 03:44:54 +0200

    creat the category template

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ app_name='store'
 urlpatterns = [
     path('', views.all_products , name='all_products'),
       path('item/<slug:slug>/', views.product_detail, name='product_detail'),
+       path('search/<slug:category_slug>/', views.category_list, name='category_list'),
     
 ]
--- store/views.py
+++ store/views.py
@@ -14,6 +14,13 @@ def categories(request):
     }
 
 
+
+def category_list(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/products/category.html', {'category': category, 'products': products})
+
+
 def product_detail(request, slug):
     '''
     a revoir
