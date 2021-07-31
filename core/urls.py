95fd2142e8b16cf287793c87c5e16283e4a64d93
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-08-01 00:56:28 +0200

    add comments to the url folder medul core

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,15 @@ from django.urls import path , include
 from django.conf import settings 
 from django.conf.urls.static import static
 
+
+# including the store.urls to get accet to the store's urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls'), name='store'), 
 ]
+
+# enfin hakda w mazal mafhamthach mlih
+# Fonction utilitaire qui renvoie un motif d’URL pour servir les fichiers en mode débogage :
 
 if settings.DEBUG : 
     urlpatterns+= static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
